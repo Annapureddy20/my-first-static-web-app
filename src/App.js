db1627babc141d8803a5044528cfc04ddc0e79ef
Annapureddy20 <144750713+Annapureddy20@users.noreply.github.com>
2023-09-11 18:53:34 -0700

    Update App.js
    cloud hosting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import Lab1Image from  './Lab1.jpg';
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return( 
+ <div
+ <p>
+            I did my homework!
+        </p>
+        <img src={Lab1Image}/>
+    </div>;
+);
 }
 
 export default App;
